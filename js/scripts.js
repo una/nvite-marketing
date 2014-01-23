2a95722969ce14e4264d7428da357c01f345eedc
Una Kravets <Una21136@yahoo.com>
2014-01-23 13:56:47 -0500

    added a CSS iPhone instead of an image of one

--- js/scripts.js
+++ js/scripts.js
@@ -13,3 +13,25 @@ $(window).scroll(function() {
 
 	styledDiv.toggleClass('popOuts', currentScroll - 550 >= targetScroll);
 });
+
+$(document).ready(function(){
+	$('.iphone__screen--1').show();
+
+	$('.popout-l-1').on('hover', function(){
+		$('.iphone__screen--2').show();
+	});
+
+	$('.popout-r-1').on('hover', function(){
+		$('.iphone__screen--3').show();
+	});
+
+	$('.popout-r-2').on('hover', function(){
+		$('.iphone__screen--4').show();
+	});
+});
+
+$(window).scroll(function() {
+	if ($('.iphone').offset().top >= 815) {
+		$('.iphone').removeClass('fixedPos').css('top', 'auto');
+	}
+});
