012f79027baf1d6020b4f1cd746e661adc91167c
Una Kravets <Una21136@yahoo.com>
2014-01-23 17:32:29 -0500

    added the ipad moving in too

--- js/scripts.js
+++ js/scripts.js
@@ -3,19 +3,21 @@ $(window).scroll(function() {
 		targetScroll = $('.iphone').position().top,
 		currentScroll = $('html').scrollTop() || $('body').scrollTop();
 
-	styledDiv.toggleClass('fixedPos', currentScroll >= targetScroll);
+	styledDiv.toggleClass('js-fixedPos', currentScroll >= targetScroll).addClass('js-screenSwitch');
+	$('.ipad').toggleClass('js-screenSwitch', currentScroll >= targetScroll);
 });
 
-$(window).scroll(function() {
-	var styledDiv = $('.iphone'),
-		targetScroll = $('.iphone').position().top,
-		currentScroll = $('html').scrollTop() || $('body').scrollTop() ;
+// $(window).scroll(function() {
+// 	var styledDiv2 = $('.iphone'),
+// 		targetScroll2 = $('.iphone').position().top,
+// 		currentScroll2 = $('html').scrollTop() || $('body').scrollTop() ;
 
-	styledDiv.toggleClass('popOuts', currentScroll - 550 >= targetScroll);
-});
+// 	styledDiv2.addClass('js-popOuts', (currentScroll2 - 800) >= targetScroll2);
+// });
 
 $(window).scroll(function() {
 	if ($('.iphone').offset().top >= 815) {
-		$('.iphone').removeClass('fixedPos').css('top', 'auto');
+		$('.iphone').removeClass('js-fixedPos').css('top', 'auto');
 	}
 });
+
